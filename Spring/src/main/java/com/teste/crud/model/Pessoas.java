43b26c99fd110b6a0de0ee16aa6bad799df260d7
Lizandropeixoto <lizandro@stj.jus.br>
2020-08-14 13:55:26 -0300

    AJuste de código

---
+++ Spring/src/main/java/com/teste/crud/model/Pessoas.java
@@ -0,0 +1,68 @@
+package com.teste.tse.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoas {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "active")
+	private boolean active;
+
+	public Pessoas() {
+	}
+
+	public Pessoas(String nome, String email) {
+		this.nome = nome;
+		this.email = email;
+		this.active = false;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [id=" + id + ", nome=" + nome + ", email=" + email + ", active=" + active + "]";
+	}
+}
