ccc767c1b04515802495aa9c81d7d92d0b5d3d78
Lizandropeixoto <lizandro@stj.jus.br>
2020-08-16 15:56:16 -0300

    Ajuste Rota do back

--- Angular/proxy.conf.js
+++ Angular/proxy.conf.js
@@ -2,7 +2,7 @@ const proxy = [
   {
     context: "/api",
     target: "http://localhost:8080",
-    pathRewrite: { "^/api": "" },
+    pathRewrite: { "^pages/api": "" },
   },
 ];
 module.exports = proxy;
